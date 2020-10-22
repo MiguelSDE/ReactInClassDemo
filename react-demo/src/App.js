8940bd47575c6989fb82c987b3a4bac1a99cb703
MikeVita4 <miguelvita13@gmail.com>
2020-10-22 22:59:48 +0000

    Initial commit

---
+++ react-demo/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TaskList from './TaskList';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>To-do List</h1>
+        <input></input>
+        <button>Add Todo</button>
+        <TaskList />
+        <TaskList />
+    </div>
+  );
+}
+
+export default App;
